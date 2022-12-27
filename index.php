95fa929e43fece73767cf4ca8ed8ad870cd1660e
Rüstem Avcı <75227416+PardusDev@users.noreply.github.com>
2022-12-27 16:58:07 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<?php   header("Refresh:0; url=admin"); ?>
+
